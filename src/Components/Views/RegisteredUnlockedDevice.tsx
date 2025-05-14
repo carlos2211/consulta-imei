a381cfc31e565c673826b4dc18d56cb527e89a36
Carlos Orrego <orregoc@globalhitss.com>
2025-05-14 12:09:57 -0400

    update Bands.

--- src/Components/Views/RegisteredUnlockedDevice.tsx
+++ src/Components/Views/RegisteredUnlockedDevice.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { translations } from "../../utils/translation";
-import img from "../../assets/img/Bands/5g.png";
+import img_5g from "../../assets/img/Bands/5g.png";
+import img_2g from "../../assets/img/Bands/no_3g_4g.png";
+import img_no3g from "../../assets/img/Bands/no_3g.png";
 import smile from "../../assets/img/smile.png";
 
 interface RegisteredUnlockedDeviceProps {
@@ -14,7 +16,8 @@ const RegisteredUnlockedDevice = ({
   data: any;
   onReset: () => void;
 }) => {
-  console.log("data", data); // Removed or moved inside the component
+  console.log("data", data);
+  console.log(data.radioDifusion.message);
   const language = "EN";
   const t = translations[language];
   return (
@@ -25,30 +28,67 @@ const RegisteredUnlockedDevice = ({
       </h1>
       <div className="flex flex-col items-center gap-4 mt-4">
         <p className="text-lg">
-          Tu equipo se encuentra <span className="text-[#008C67] font-bold">desbloqueado y esta inscrito en el sistema</span> y
-          puede operar en todas las redes móviles nacionales 2G 3G 4G.
+          Tu equipo se encuentra{" "}
+          <span className="text-[#008C67] font-bold">
+            desbloqueado y esta inscrito en el sistema
+          </span>{" "}
+          y puede operar en todas las redes móviles nacionales{" "}
+          {data.homologacion}.
         </p>
-        <p className="text-lg">
-          CLARO no tiene información respecto a las características 5G de este
-          equipo. Consulte con la empresa donde lo adquirió para verificar
-          compatibilidad 5G en cada compañía.
-        </p>
-        <img src={img} alt="5G" className="w-48 h-24" />
-        <p className="text-lg">
-          Este equipo <span className="text-[#DA291C] font-bold">no tiene</span> incorporado un sintonizador de radio FM
-        </p>
-           <div className="bg-[#F5F7FC] py-6 px-8 rounded-3xl mt-4 ">
-        <p className="font-bold my-2">
-          Todos los IMEI de los equipos móviles, tanto teléfonos como otros
-          dispositivos, deberán estar inscritos en un sistema centralizado para
-          poder operar en las redes nacionales.
-        </p>
-        <p>Multibanda SAE – Ver información detallada en <a href="https://multibanda.cl" className="text-[#0097A9] underline font-bold line">https://multibanda.cl</a></p>
+        {data.homologacion === "5G e inferiores" ? (
+          <p className="text-lg">
+            CLARO no tiene información respecto a las características 5G de este
+            equipo. Consulte con la empresa donde lo adquirió para verificar
+            compatibilidad 5G en cada compañía.
+          </p>
+        ) : null}
+        <img
+          src={
+            data.homologacion === "5G e inferiores" || "4G e inferiores"
+              ? img_5g
+              : data.homologacion === "2G"
+              ? img_2g
+              : data.homologacion === "4G y 2G, sin soporte de 3G"
+              ? img_no3g
+              : ""
+          }
+          alt="Homologación"
+          className="w-48 h-24"
+        />
+        {data.radioDifusion.message === "NO" ? (
+          <p className="text-lg">
+            Este equipo{" "}
+            <span className="text-[#DA291C] font-bold">no tiene </span>
+            incorporado un sintonizador de radio FM.
+          </p>
+        ) : data.radioDifusion.message === "SI" ? (
+          <p className="text-lg">
+            Este equipo{" "}
+            <span className="text-[#008C67] font-bold">sí tiene </span>
+            incorporado un sintonizador de radio FM.
+          </p>
+        ) : data.radioDifusion.message === "NA" ? (
+          <p className="text-lg">
+            No hay información de sintonizador de radio FM para este equipo.
+          </p>
+        ) : null}
+        <div className="bg-[#F5F7FC] py-6 px-8 rounded-3xl mt-4 ">
+          <p className="font-bold my-2">
+            Todos los IMEI de los equipos móviles, tanto teléfonos como otros
+            dispositivos, deberán estar inscritos en un sistema centralizado
+            para poder operar en las redes nacionales.
+          </p>
+          <p>
+            Multibanda SAE – Ver información detallada en{" "}
+            <a
+              href="https://multibanda.cl"
+              className="text-[#0097A9] underline font-bold line"
+            >
+              https://multibanda.cl
+            </a>
+          </p>
         </div>
-        <button
-          className="red-button "
-          onClick={onReset}
-        >
+        <button className="red-button " onClick={onReset}>
           Consultar otro IMEI
         </button>
       </div>
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/consulta_imei/consulta-imei-migracion/dist/',
 })
